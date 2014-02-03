eabb83932252333e3704d049992f60e4659abf81
Chevalier Maxime <maxime.chevalier@iut-valence.fr>
2014-02-03 17:35:15 +0100

    mon premier commit

---
+++ src/Test.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+    public static void main(String[] args) {
+	
+  
+    	if (args.length>=0)
+    		   					
+    	System.out.print(args[0]);
+    	
+    	else
+    		System.err.print("pas d'arguments");
+    
+    
+    }
+    
+}
