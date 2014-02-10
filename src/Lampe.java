fafbd4d005b5b5f03f96554c3079e5b3bd661713
Chevalier Maxime <maxime.chevalier@iut-valence.fr>
2014-02-10 17:25:06 +0100

    Java doc

---
+++ src/Lampe.java
@@ -0,0 +1,44 @@
+
+public class Lampe
+{
+	/** Lampe poseede deux Methodes, allumer et Eteindre.
+	 * 
+	 */
+public boolean estAllumee;
+	
+public int puissance;
+
+/**Lampe() constructeur par défaut.
+ * Puissance = 100
+ * Eteinte pas defaut 
+ */
+public Lampe()
+{
+	this.estAllumee = false;
+	this.puissance = 100;
+
+}
+
+/**Allumer() Allume la lampe
+ * 
+ */
+
+public void Allume() {
+	this.estAllumee = true;	
+}
+
+
+/**Eteindre() Eteind la lampe
+ * 
+ */
+public void Eteindre(){
+	this.estAllumee = false;
+}
+
+	
+}
+
+
+
+
+
